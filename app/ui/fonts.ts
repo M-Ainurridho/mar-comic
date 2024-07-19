062d889d5d91743b7779eed87619c26b3c7f7314
M-Ainurridho <m.ainurridho11@gmail.com>
2024-07-19 22:27:32 +0700

    add module clsx, and set custom font

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Poppins, Roboto } from "next/font/google";
+
+export const poppins = Poppins({
+   weight: ["400", "500", "600", "700"],
+   subsets: ["latin"],
+});
+
+export const roboto = Roboto({
+   weight: ["100", "300"],
+   subsets: ["latin"],
+});
