42a8d829fe6bdf93194ffbdf03433f213f9fffc4
M-Ainurridho <m.ainurridho11@gmail.com>
2024-07-22 22:24:50 +0700

    fixed ui landing page, kecuali search box

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import Navbar from "@/app/ui/navbar";
-import SearchBox from "@/app/ui/home/search-box";
+import Search from "@/app/ui/home/search";
 
 export const metadata: Metadata = {
    title: "Home | MAR Comic",
@@ -10,10 +10,10 @@ const Home = () => {
    return (
       <main className="h-screen bg-gradient-to-br from-purple-300 via-cyan-400 to-pink-300">
          <Navbar />
-         <div className="h-[88%] flex items-center justify-center ">
-            <div className="w-1/2">
+         <div className="h-[88%] flex items-center justify-center">
+            <div className="w-1/2 h-2/5">
                <h1 className="font-bold text-6xl text-white mb-4">Search Manga..</h1>
-               <SearchBox />
+               <Search />
             </div>
          </div>
       </main>
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -6,7 +6,7 @@ import { Bars3Icon } from "@heroicons/react/24/outline";
 
 const Navbar = () => {
    return (
-      <nav className="p-4 h-[12%]">
+      <nav className="py-6 px-12 h-[12%]">
          <div className="flex items-center justify-between">
             <div className="nav-left">
                <Bars3Icon className="block md:hidden w-8 cursor-pointer" />
@@ -18,7 +18,7 @@ const Navbar = () => {
             <NavLink />
             <div className="nav-right flex items-center gap-x-4">
                {/* <MagnifyingGlassIcon className="w-9 border border-neutral-700 p-2 rounded-full cursor-pointer hover:bg-neutral-100 duration-100" /> */}
-               <Link href="/signin" className="bg-gray-600 hover:bg-gray-500 duration-100 text-white px-4 py-2 rounded-full">
+               <Link href="/signin" className="bg-gray-600 hover:bg-gray-500 duration-100 text-white font-medium px-4 py-2 rounded-full shadow-md shadow-neutral-400">
                   Sign in
                </Link>
             </div>
